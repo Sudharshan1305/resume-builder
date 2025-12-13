7d18161ba7a97d297e595140ace002861e63ea70
Sudharshan1305 <sudharshankrishnaalk@gmail.com>
2025-12-13 19:28:48 +0530

    Fix catch-all route pattern for Express compatibility

--- server/server.js
+++ server/server.js
@@ -123,7 +123,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // IMPORTANT: Catch-all route MUST BE LAST
 // This handles SPA routing - serve index.html for all non-API routes
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
